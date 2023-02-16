4aca76059331c57d086b2c14b8168fe11929b36e
Aline Benjamim <alineoak9@gmail.com>
2023-02-16 16:13:16 -0300

    fix: Refatoração endpoint getPost

--- src/business/PostBusiness.ts
+++ src/business/PostBusiness.ts
@@ -1,5 +1,5 @@
 import { PostDatabase } from "../database/PostDatabase"
-import { CreatePostInput, CreatePostOutput } from "../dtos/postDTO"
+import { CreatePostInput, CreatePostOutput, GetPostsInput } from "../dtos/postDTO"
 import { BadRequestError } from "../errors/BadRequestError"
 import { Post } from "../models/Post"
 import { IdGenerator } from "../services/IdGenerator"
@@ -10,7 +10,23 @@ export class PostBusiness {
               private idGenerator: IdGenerator,
               private tokenManager: TokenManager) {}
 
-  public getPosts = async (q: string | undefined) => {
+  public getPosts = async (input: GetPostsInput) => {
+    const {q, token} = input
+
+    if(!token){
+      throw new BadRequestError("Token não enviado!")
+    }
+
+    const payload = this.tokenManager.getPayload(token as string)
+
+    if(payload === null){
+      throw new BadRequestError("Token inválido!")
+    }
+
+    if(typeof q !== "string" && q !== undefined){
+      throw new BadRequestError("'q' deve ser string ou undefined")
+    }
+
     const { postsDB, usersDB } = await this.postDatabase.getPostsAndUsers(q)
 
     const posts = postsDB.map((postDB) => {
--- src/dtos/postDTO.ts
+++ src/dtos/postDTO.ts
@@ -1,9 +1,13 @@
 export interface CreatePostInput {
     content: string,
     token: string | undefined
-
 }
 
 export interface CreatePostOutput {
     message: string
+}
+
+export interface GetPostsInput{
+    q: unknown,
+    token: string | undefined
 }
